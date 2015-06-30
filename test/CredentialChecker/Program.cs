71cb58e9165cbfb71b95d838d1b01a4efc650924
wtijsma <wiebe@tijsma.com>
2015-06-30 18:38:35 +0200

    Added support for enumerating all credentials using AdvApi32.CredEnumerate

--- test/CredentialChecker/Program.cs
+++ test/CredentialChecker/Program.cs
@@ -7,10 +7,29 @@ namespace CredentialChecker
     {
         static void Main(string[] args)
         {
+            //WriteSingleCredential();
+
+            WriteAllCredentials();
+
+            Console.ReadLine();
+        }
+
+        private static void WriteSingleCredential()
+        {
             var credential = new Credential {Target = "Test"};
             credential.Load();
 
             Console.WriteLine("User name: {0}, Password: {1}", credential.Username, credential.Password);
+        }
+
+        private static void WriteAllCredentials()
+        {
+            var loadAll = Credential.LoadAll();
+
+            foreach (var credential1 in loadAll)
+            {
+                Console.WriteLine(credential1);
+            }
         }
     }
 }
